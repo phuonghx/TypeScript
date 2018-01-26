626b584413be24897af7c645de1656d2f1cb7b47
Sheetal Nandi <shkamat@microsoft.com>
2018-01-26 14:06:22 -0800

    Replace usage of jsonObject on JsonSourceFile

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -2568,8 +2568,12 @@ namespace ts {
     }
 
     export interface JsonSourceFile extends SourceFile {
-        jsonObject?: ObjectLiteralExpression;
+        statements: NodeArray<JsonObjectLiteralExpressionStatement>;
         extendedSourceFiles?: string[];
+    }
+
+    export interface JsonObjectLiteralExpressionStatement extends ExpressionStatement {
+        expression: ObjectLiteralExpression;
     }
 
     export interface ScriptReferenceHost {
